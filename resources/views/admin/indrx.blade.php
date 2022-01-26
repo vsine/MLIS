3c7d42b59a0670b93fbb1d4d53cc1cf6eadd0254
vsine <neoway@126.com>
2022-01-26 12:07:29 +0800

    update

--- resources/views/admin/indrx.blade.php
+++ resources/views/admin/indrx.blade.php
@@ -1,3 +1,6 @@
 @extends("public.base")
 
 
+@section('body')
+   <h1>你好世界</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',function (){
-    return view('public.base');
+Route::prefix('admin')->group(function (){
+    Route::get('/',function (){
+        return view('admin.indrx');
+    });
 });
