67eafa3608a329ad282e2d507f7b987c132c03e8
vsine <neoway@126.com>
2022-01-26 12:26:09 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('',function (){
+    return view('welcome');
+});
 Route::prefix('admin')->group(function (){
     Route::get('',function (){
         return view('admin.indrx');
+    })->name('admin.index');
+    Route::fallback(function (){
+        return redirect('');
     });
 });
+
+
+Route::fallback(function (){
+    return redirect('');
+});
+
