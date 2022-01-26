4c051194b20118bb2943d5feafc1cacad9baa914
vsine <neoway@126.com>
2022-01-27 05:00:28 +0800

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers;
 
+
+use Illuminate\Http\Request;
+
 class LoginController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $request->validate(['username'=>'required|min:5',
+            'password'=>'required|min:5'
+        ]);
+
+        return redirect('admin');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('check')->get('user', function () {
     return view('welcome');
 });
+
+Route::get('login',[\App\Http\Controllers\LoginController::class,'index']);
