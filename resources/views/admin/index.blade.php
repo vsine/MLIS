ab4e83c3a5ce042293221ec0f8484c801a42c172
vsine <neoway@126.com>
2022-01-30 17:13:22 +0800

    update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -874,75 +874,12 @@
     <!-- BEGIN MAIN CONTENT -->
 
 
-    <section id="main-content">
+    <div id="main-content">
         <!-- BEGIN WRAPPER  -->
         <section class="wrapper">
             <!-- BEGIN ROW  -->
             <div class="row state-overview">
-                <div class="col-lg-3 col-sm-6">
-                    <section class="panel">
-                        <div class="symbol">
-                            <i class="fa fa-tags blue">
-                            </i>
-                        </div>
-                        <div class="value">
-                            <h1 class="count">
-                                0
-                            </h1>
-                            <p>
-                                Total Sale
-                            </p>
-                        </div>
-                    </section>
-                </div>
-                <div class="col-lg-3 col-sm-6">
-                    <section class="panel">
-                        <div class="symbol">
-                            <i class="fa fa-money red">
-                            </i>
-                        </div>
-                        <div class="value">
-                            <h1 class=" count2">
-                                0
-                            </h1>
-                            <p>
-                                Total Profit
-                            </p>
-                        </div>
-                    </section>
-                </div>
-                <div class="col-lg-3 col-sm-6">
-                    <section class="panel">
-                        <div class="symbol">
-                            <i class="fa fa-user yellow">
-                            </i>
-                        </div>
-                        <div class="value">
-                            <h1 class=" count3">
-                                0
-                            </h1>
-                            <p>
-                                New Users
-                            </p>
-                        </div>
-                    </section>
-                </div>
-                <div class="col-lg-3 col-sm-6">
-                    <section class="panel">
-                        <div class="symbol">
-                            <i class="fa fa-shopping-cart purple">
-                            </i>
-                        </div>
-                        <div class="value">
-                            <h1 class=" count4">
-                                0
-                            </h1>
-                            <p>
-                                New Orders
-                            </p>
-                        </div>
-                    </section>
-                </div>
+
             </div>
             <!-- END ROW  -->
             <div id="morris">
@@ -955,7 +892,7 @@
             <!-- END ROW  -->
         </section>
         <!-- END WRAPPER  -->
-    </section>
+    </div>
     <!-- END MAIN CONTENT -->
     <!-- BEGIN FOOTER -->
     <footer class="site-footer">
