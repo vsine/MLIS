76ae4edd753c832ffd5e5832371a99f556b6daa1
vsine <neoway@126.com>
2022-01-31 12:21:12 +0800

    init

--- app/Http/Middleware/TaskMiddleware.php
+++ app/Http/Middleware/TaskMiddleware.php
@@ -4,13 +4,21 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class TaskMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-
+        $username=Session::get('username');
+        $last_token=DB::table('users')->where('username',$username)
+            ->value('last_token');
+        if (Session::token()==$last_token){
+            Session::put('task_check',true);
+        }else{
+            Session::put('task_check',false);
+        }
         return $next($request);
     }
-
 }
