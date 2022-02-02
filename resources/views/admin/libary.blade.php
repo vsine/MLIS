90b287b705a198b9ccbf26e7dcf66c771ebfa13b
vsine <neoway@126.com>
2022-02-02 17:32:44 +0800

    init

--- resources/views/admin/libary.blade.php
+++ resources/views/admin/libary.blade.php
@@ -1,1 +1,97 @@
-<?php
+<!-- BEGIN WRAPPER  -->
+<link href="/css/style-responsive.css" rel="stylesheet"><!-- THEME RESPONSIVE CSS -->
+<link href="/css/table-responsive.css" rel="stylesheet"><!-- TABLE RESPONSIVE CSS -->
+<section class="wrapper">
+    <!-- BEGIN ROW  -->
+    <div class="row">
+        <div class="col-lg-12">
+            <section class="panel">
+                <header class="panel-heading">
+                  <span class="label label-primary">
+                   仓库
+                  </span>
+                    <span class="tools pull-right">
+                    <a href="javascript:;" class="fa fa-chevron-down">
+                    </a>
+                  </span>
+                </header>
+                <div class="panel-body">
+                    <section id="unseen">
+                        <table class="table table-bordered table-striped table-condensed">
+                            <thead>
+                            <tr>
+                                <th>
+                                    #编号
+                                </th>
+                                <th>
+                                    名称
+                                </th>
+                                <th class="numeric">
+                                    类别
+                                </th>
+                                <th class="numeric">
+                                    品牌
+                                </th>
+                                <th class="numeric hidden-phone">
+                                    型号
+                                </th>
+                                <th class="numeric hidden-phone">
+                                    数量
+                                </th>
+                                <th class="numeric hidden-phone">
+                                    货位
+                                </th>
+                                <th class="numeric hidden-phone">
+                                    供应商
+                                </th>
+                                <th class="numeric">
+                                    操作
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>
+                                    AAC
+                                </td>
+                                <td>
+                                    AUSTRALIAN AGRICULTURAL COMPANY LIMITED.
+                                </td>
+                                <td class="numeric">
+                                    $1.38
+                                </td>
+                                <td class="numeric">
+                                    -0.01
+                                </td>
+                                <td class="numeric hidden-phone">
+                                    -0.36%
+                                </td>
+                                <td class="numeric hidden-phone">
+                                    $1.39
+                                </td>
+                                <td class="numeric hidden-phone">
+                                    $1.39
+                                </td>
+                                <td class="numeric hidden-phone">
+                                    $1.38
+                                </td>
+                                <td class="numeric">
+                                    9,395
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </section>
+                </div>
+            </section>
+        </div>
+    </div>
+    <!-- END ROW  -->
+</section>
+<!-- END WRAPPER  -->
+@section('script')
+    <script type="text/javascript">
+        //alert('0');
+    </script>
+@endsection
