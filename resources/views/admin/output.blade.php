4efd0602a5a896680232897f1f9b326bd4b68673
xsine <neoway@126.com>
2022-02-18 19:26:55 +0800

    update

--- resources/views/admin/output.blade.php
+++ resources/views/admin/output.blade.php
@@ -1,1 +1,36 @@
-<?php
+<section class="wrapper">
+    <!-- BEGIN ROW  -->
+    <div class="row">
+        <div class="col-lg-12">
+
+
+
+
+            <section class="panel">
+                <header class="panel-heading tab-bg-dark-navy-blue ">
+                    <ul class="nav nav-tabs">
+                        <li class="active">
+                            <a data-toggle="tab" href="#home">材料车</a>
+                        </li>
+                        <li class="">
+                            <a data-toggle="tab" href="#about">订单</a>
+                        </li>
+                    </ul>
+                </header>
+                <div class="panel-body">
+                    <div class="tab-content">
+                        <div id="home" class="tab-pane active">
+                            Home
+                        </div>
+                        <div id="about" class="tab-pane">About</div>
+
+                    </div>
+                </div>
+            </section>
+
+
+        </div>
+    </div>
+
+    <!-- END ROW  -->
+</section>
