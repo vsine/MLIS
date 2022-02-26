d847fa710eac3886f8df5ed6c846c1ec67e12f87
xsine <neoway@126.com>
2022-02-26 15:02:45 +0800

    update

--- resources/views/admin/output.blade.php
+++ resources/views/admin/output.blade.php
@@ -119,8 +119,8 @@
             </div>
 
                 <div class="row">
-                    <div class="col-sm-1"></div>
-                    <div class="col-sm-10" >
+                    <div class="col-sm-1 col-xs-1"></div>
+                    <div class="col-sm-10 col-xs-10" >
                         <table class="table table-bordered">
                             <tbody>
                             <tr>
@@ -184,7 +184,7 @@
 
                     </div>
 
-                    <div class="col-sm-1"></div>
+                    <div class="col-sm-1 col-xs-1"></div>
                 </div>
 
             </div>
