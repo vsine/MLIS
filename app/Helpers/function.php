13867690851323ae23088703eb8094eaae12add8
xsine <neoway@126.com>
2022-03-06 00:48:43 +0800

    update

--- app/Helpers/function.php
+++ app/Helpers/function.php
@@ -1,14 +1,17 @@
 <?php
-
+/**
+ * @param $key
+ * @param $value
+ * @return false|string
+ * @developer Mao.
+ */
 function setParams($key,$value){
     $url=URL::full();
     if (strpos($url,'?'))
         if (strpos($url,$key)&&strlen($key)==(strrpos($url,'=',strpos($url,$key))-strpos($url,$key)))
-            $url=substr($url,0,strrpos($url,$key)+strlen(strrpos($url,$key))+1).
-                $value
+            $url=substr($url,0,strrpos($url,$key)+strlen(strrpos($url,$key))+1). $value
                 .strrpos($url,'&',strrpos($url,$key)+strlen(strrpos($url,$key)))?
-                substr($url,strrpos($url,'&',strrpos($url,$key)+strlen(strrpos($url,$key))),strlen($url))
-                :'';
+                substr($url,strrpos($url,'&',strrpos($url,$key)+strlen(strrpos($url,$key))),strlen($url)) :'';
         else
             $url=$url.'&'.$key.'='.$value;
     else
