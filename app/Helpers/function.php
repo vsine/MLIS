ba4c7b14b321e2048c0aec8c9750eb07f468bbac
xsine <neoway@126.com>
2022-03-06 01:27:59 +0800

    update

--- app/Helpers/function.php
+++ app/Helpers/function.php
@@ -9,13 +9,12 @@ function setParams($key,$value){
     $url=URL::full();
     $last_point=0;
     if (strpos($url,'?')){
-        do{
-        if (strpos($url,$key,$last_point)&&strlen($key)==(strpos($url,'=',strpos($url,$key,$last_point))-strpos($url,$key,$last_point)))
+        do if (strpos($url,$key,$last_point)&&strlen($key)==(strpos($url,'=',strpos($url,$key,$last_point))-strpos($url,$key,$last_point)))
            return $url = substr($url,0,strpos($url,$key,$last_point)+strlen($key)+1). $value
                    .(strpos($url,'&',strpos($url,$key,$last_point)+strlen(strpos($url,$key,$last_point)))?
                    substr($url,strpos($url,'&',strpos($url,$key,$last_point)
                    +strlen(strpos($url,$key,$last_point))),strlen($url)) :'');
-        }while($last_point=strpos($url,$key,$last_point+strlen($key)));
+       while($last_point=strpos($url,$key,$last_point+strlen($key)));
        return $url=$url.'&'.$key.'='.$value;
     }
     else
